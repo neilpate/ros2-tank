4fde4da643a0c3016459cee8ee0d04bbef3941fe
Neil Pate <neil@premiergsolutions.com>
2026-01-04 14:57:49 +0100

    fix: Remove dependency on pytest so the nodes build without errors
    Build runs ok on windows now

--- src/motion_controller/setup.py
+++ src/motion_controller/setup.py
@@ -16,7 +16,6 @@ setup(
     maintainer_email="neil@premiergsolutions.com",
     description="TODO: Package description",
     license="TODO: License declaration",
-    tests_require=["pytest"],
     entry_points={
         "console_scripts": ["motion_controller_node = motion_controller.main:main"],
     },
--- src/user_interface/setup.py
+++ src/user_interface/setup.py
@@ -17,7 +17,6 @@ setup(
     maintainer_email='neil@premiergsolutions.com',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'user_interface_node = user_interface.main:main'
